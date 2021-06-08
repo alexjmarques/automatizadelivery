246e2c833bdbaaf73ba2ca00456f9548608854fd
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-06-08 18:52:27 -0300

    Update UsuarioController.php

--- app/controller/UsuarioController.php
+++ app/controller/UsuarioController.php
@@ -158,15 +158,13 @@ class UsuarioController extends Controller
                 if($contagem > 0){
                     $usuId = $this->acoes->getByField('usuariosEmpresa', 'id_usuario', $usuario->id);
                     $empresa = $this->acoes->getByField('empresa', 'id', $usuId->id_empresa);
-
-                    dd('aqui');
                     header('Content-Type: application/json');
                     $json = json_encode(['id' => 1, 'url' => "{$empresa->link_site}/admin", 'resp' => 'login', 'mensagem' => "Aguarde estamos redirecionando para a pagina inicial"]);
                 }else{
                     $usuId = $this->acoes->getByField('usuariosEmpresa', 'id_usuario', $usuario->id);
                     $empresa = $this->acoes->getByField('empresa', 'id', $usuId->id_empresa);
                     header('Content-Type: application/json');
-                    $json = json_encode(['id' => 1, 'url' => "{$empresa->link_site}/admin", 'mensagem' => "Aguarde estamos redirecionando para a pagina inicial"]);
+                    $json = json_encode(['id' => 1, 'url' => "admin", 'mensagem' => "Aguarde estamos redirecionando para a pagina inicial"]);
                 }
             } else {
                 header('Content-Type: application/json');
