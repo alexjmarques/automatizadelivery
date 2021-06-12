c75c399c7caedb6814770d48b290010e7b70e57f
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-06-12 02:47:54 -0300

    Update AllController.php

--- app/controller/AllController.php
+++ app/controller/AllController.php
@@ -314,8 +314,8 @@ class AllController extends Controller
         $usuario = $this->acoes->getByField('usuariosEmpresa', 'id_usuario', $idUsuario);
         if($usuario){
             $empresa = $this->acoes->getByField('empresa', 'id', $usuario->id_empresa);
-
             if($idEmpresa != $empresa->id_empresa){
+                dd($empresa->id_empresa);
                 redirect(BASE."{$empresa->link_site}/admin");
             }
         }
