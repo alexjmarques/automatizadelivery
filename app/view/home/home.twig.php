52363ec624c783d98bf4263cdb079da4e3eb2328
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-06-19 20:32:17 -0300

    Update home.twig.php

--- app/view/home/home.twig.php
+++ app/view/home/home.twig.php
@@ -24,7 +24,7 @@
 <section class="section pt-5 pb-5 bg-white homepage-add-section">
    <div class="container">
       <div class="row">
-         {% for emp in empresas|slice(0,6) %}
+         {% for emp in empresas|slice(2,7) %}
          <div class="col-md-4">
             <a href="{{BASE}}{{ emp.link_site }}">
                <div class="bg-white shadow-sm rounded align-items-center p-1 mb-4 flex-column">
