fbef717c2a7f1d463187bdcac0667cbabaf9baa7
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-06-29 08:23:11 -0300

    Update UsuarioController.php

--- app/controller/UsuarioController.php
+++ app/controller/UsuarioController.php
@@ -809,7 +809,7 @@ class UsuarioController extends Controller
                 exit($json);
             } else {
                 header('Content-Type: application/json');
-                $json = json_encode(['id' => 0, 'resp' => 'insert', 'mensagem' => 'Informe o número juntamente com o endereço digitado', 'erro' => 'Informe o número juntamente com o endereço digitado']);
+                $json = json_encode(['id' => 0, 'resp' => 'insert', 'error' => 'Informe o número juntamente com o endereço digitado']);
                 exit($json);
             }
         }
