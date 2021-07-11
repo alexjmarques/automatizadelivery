25a9c0e1121155bbaa051f8236aac5574635339c
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-07-11 20:16:17 -0300

    Update categoria.twig.php

--- app/view/home/mobile/categoria.twig.php
+++ app/view/home/mobile/categoria.twig.php
@@ -18,12 +18,12 @@
 {% set idTamanhos = tc.id_tamanhos %}
 {% endif %}
 {% endfor %}
-    {% if c.id == idCategoria %}
-    {% if c.produtos > 0 %}
-    <div class="px-3 pt-3 title d-flex bg-white">
-        <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
+{% if c.id == idCategoria %}
+{% if c.produtos > 0 %}
+<div class="px-3 pt-3 title d-flex bg-white">
+    <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
 </div>
-    <div class="px-3 pt-3 title bg-white">
+<div class="px-3 pt-3 title bg-white">
     {% for tc in tamanhosCategoria %}
     {% if c.id == tc.id_categoria %}
     {% for tam in tamanhos %}
@@ -37,134 +37,132 @@
                     <h6 class="mb-1">
                         <a href="{{BASE}}{{empresa.link_site}}/{{c.slug}}/produto/{{tc.id}}/{{tam.id}}/{{i}}" class="text-black text-uppercase">
                             Pizza {{tam.nome}} {% if i == 1 %}{{i}} SABOR {% else %}{{i}} SABORES {% endif %}
-                            </a>
+                        </a>
                     </h6>
-                    <p class="text-gray mb-0 pb-0">Esta pizza tem {% if i == 1 %} {{tam.qtd_pedacos}} pedaço {% else %} {{tam.qtd_pedacos}} pedaços {% endif %}</p>
+                    <p class="text-gray mb-0 pb-0">Esta pizza tem {% if tam.qtd_pedacos == 1 %} {{tam.qtd_pedacos}} pedaço {% else %} {{tam.qtd_pedacos}} pedaços {% endif %}</p>
                 </div>
             </div>
         </div>
     </div>
     {% endfor %}
 
-
     {% endif %}
     {% endfor %}
     {% endif %}
     {% endfor %}
-  
     <div class="clearfix"></div>
+</div>
+{% endif %}
+
+{% else %}
+
+{% if c.produtos > 1 %}
+<div class="px-3 pt-3 title d-flex bg-white">
+    <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
+    <!-- <a class="font-weight-bold ml-auto" href="trending.html">Ver todos <i class="feather-chevrons-right"></i></a> -->
+</div>
+
+<div class="trending-slider bg-white linha">
+    {% for p in produto %}
+    {% if c.id == p.id_categoria %}
+    {% if hoje in p.dias_disponiveis %}
+    {% if veriFavoritos == 0 %}
+    <div class="osahan-slider-item py-3 px-1">
+        <div class="list-card bg-white h-100 rounded overflow-hidden position-relative">
+            <div class="list-card-image">
+                {% if p.valor_promocional != '0.00' %}
+                <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> DESCONTO</span></div>
+                {% endif %}
+
+                {% if p.status == 0 %}
+                <div class="star position-absolute"><span class="badge badge-secondary">ESGOTADO</span></div>
+                {% endif %}
+                <div class="favourite-heart text-danger position-absolute">
+                    <a href="#" class="addFavorito" data-favorito="{{p.id}}"><i id="itFavorito{{p.id}}" class="fa fa-heart-o"></i></a>
+                </div>
+                {% if p.imagem is not empty %}
+                <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}">
+
+                    <img src="{{BASE}}uploads/{{ p.imagem }}" class="img-fluid item-img w-100">
+
+                </a>
+                {% endif %}
+            </div>
+            <div class="p-2 position-relative">
+                <div class="list-card-body">
+                    <h6 class="mb-1">
+                        <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}" class="text-black">
+                            {{p.nome}}</a>
+                    </h6>
+                    <p class="text-gray mb-3">{{p.descricao}}</p>
+
+                    {% if p.valor_promocional != '0.00' %}
+                    <p class="text-black mb-1 dequanto pmais"><span class="float-left por">De</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
+                    <p class="text-black mb-1 porquanto pmais"> <span class="float-left por">Por</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor_promocional|number_format(2, ',', '.') }}</span></p>
+                    {% else %}
+                    <p class="text-black mb-1 porquanto pmais"> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
+                    {% endif %}
+                </div>
+            </div>
+        </div>
     </div>
+    {% else %}
+    {% for fav in favoritos %}
+    <div class="osahan-slider-item py-3 px-1">
+        <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
+            <div class="list-card-image">
+                {% if p.valor_promocional != '0.00' %}
+                <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> DESCONTO</span></div>
+                {% endif %}
+
+                {% if p.status == 0 %}
+                <div class="star position-absolute"><span class="badge badge-secondary">ESGOTADO</span></div>
+                {% endif %}
+                <div class="favourite-heart text-danger position-absolute">
+
+                    <a href="#" class="addFavorito" data-favorito="{{p.id}}"><i id="itFavorito{{p.id}}" class="fa {% if fav.id_produto == p.id %} fa-heart{% else %}fa-heart-o{% endif %}"></i></a>
+                </div>
+                {% if p.imagem is not empty %}
+                <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}">
+                    <img src="{{BASE}}uploads/{{ p.imagem }}" class="img-fluid item-img w-100">
+                </a>
+                {% endif %}
+            </div>
+            <div class="p-3 position-relative">
+                <div class="list-card-body">
+                    <h6 class="mb-1">
+                        {% if isLogin is empty %}
+                        <a href="{{BASE}}{{empresa.link_site}}/login" class="text-black">
+                            {% else %}
+                            <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}" class="text-black">
+                                {% endif %}{{p.nome}}</a>
+                    </h6>
+                    <p class="text-gray mb-3">{{p.descricao}}</p>
+
+                    {% if p.valor_promocional != '0.00' %}
+                    <p class="text-black mb-1 dequanto"><span class="float-left por">De</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
+                    <p class="text-black mb-1 porquanto pmais"> <span class="float-left por">Por</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor_promocional|number_format(2, ',', '.') }}</span></p>
+                    {% else %}
+                    <p class="text-black mb-1 porquanto pmais"> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
+                    {% endif %}
+                </div>
+            </div>
+        </div>
+    </div>
+    {% endfor %}
     {% endif %}
 
-    {% else %}
+    {% endif %}
 
-    {% if c.produtos > 1 %}
-    <div class="px-3 pt-3 title d-flex bg-white">
-        <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
-        <!-- <a class="font-weight-bold ml-auto" href="trending.html">Ver todos <i class="feather-chevrons-right"></i></a> -->
-    </div>
-
-        <div class="trending-slider bg-white linha">
-        {% for p in produto %}
-        {% if c.id == p.id_categoria %}
-        {% if hoje in p.dias_disponiveis %}
-        {% if veriFavoritos == 0 %}
-        <div class="osahan-slider-item py-3 px-1">
-            <div class="list-card bg-white h-100 rounded overflow-hidden position-relative">
-                <div class="list-card-image">
-                    {% if p.valor_promocional != '0.00' %}
-                    <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> DESCONTO</span></div>
-                    {% endif %}
-
-                    {% if p.status == 0 %}
-                    <div class="star position-absolute"><span class="badge badge-secondary">ESGOTADO</span></div>
-                    {% endif %}
-                    <div class="favourite-heart text-danger position-absolute">
-                        <a href="#" class="addFavorito" data-favorito="{{p.id}}"><i id="itFavorito{{p.id}}" class="fa fa-heart-o"></i></a>
-                    </div>
-                    {% if p.imagem is not empty %}
-                    <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}">
-
-                        <img src="{{BASE}}uploads/{{ p.imagem }}" class="img-fluid item-img w-100">
-
-                    </a>
-                    {% endif %}
-                </div>
-                <div class="p-2 position-relative">
-                    <div class="list-card-body">
-                        <h6 class="mb-1">
-                            <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}" class="text-black">
-                                {{p.nome}}</a>
-                        </h6>
-                        <p class="text-gray mb-3">{{p.descricao}}</p>
-
-                        {% if p.valor_promocional != '0.00' %}
-                        <p class="text-black mb-1 dequanto pmais"><span class="float-left por">De</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
-                        <p class="text-black mb-1 porquanto pmais"> <span class="float-left por">Por</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor_promocional|number_format(2, ',', '.') }}</span></p>
-                        {% else %}
-                        <p class="text-black mb-1 porquanto pmais"> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
-                        {% endif %}
-                    </div>
-                </div>
-            </div>
-        </div>
-        {% else %}
-        {% for fav in favoritos %}
-        <div class="osahan-slider-item py-3 px-1">
-            <div class="list-card bg-white h-100 rounded overflow-hidden position-relative shadow-sm">
-                <div class="list-card-image">
-                    {% if p.valor_promocional != '0.00' %}
-                    <div class="star position-absolute"><span class="badge badge-success"><i class="feather-star"></i> DESCONTO</span></div>
-                    {% endif %}
-
-                    {% if p.status == 0 %}
-                    <div class="star position-absolute"><span class="badge badge-secondary">ESGOTADO</span></div>
-                    {% endif %}
-                    <div class="favourite-heart text-danger position-absolute">
-
-                        <a href="#" class="addFavorito" data-favorito="{{p.id}}"><i id="itFavorito{{p.id}}" class="fa {% if fav.id_produto == p.id %} fa-heart{% else %}fa-heart-o{% endif %}"></i></a>
-                    </div>
-                    {% if p.imagem is not empty %}
-                    <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}">
-                        <img src="{{BASE}}uploads/{{ p.imagem }}" class="img-fluid item-img w-100">
-                    </a>
-                    {% endif %}
-                </div>
-                <div class="p-3 position-relative">
-                    <div class="list-card-body">
-                        <h6 class="mb-1">
-                            {% if isLogin is empty %}
-                            <a href="{{BASE}}{{empresa.link_site}}/login" class="text-black">
-                                {% else %}
-                                <a href="{{BASE}}{{empresa.link_site}}/produto/{{p.id}}" class="text-black">
-                                    {% endif %}{{p.nome}}</a>
-                        </h6>
-                        <p class="text-gray mb-3">{{p.descricao}}</p>
-
-                        {% if p.valor_promocional != '0.00' %}
-                        <p class="text-black mb-1 dequanto"><span class="float-left por">De</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
-                        <p class="text-black mb-1 porquanto pmais"> <span class="float-left por">Por</span> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor_promocional|number_format(2, ',', '.') }}</span></p>
-                        {% else %}
-                        <p class="text-black mb-1 porquanto pmais"> <span class="float-left text-black-50"> {{moeda.simbolo}} {{ p.valor|number_format(2, ',', '.') }}</span></p>
-                        {% endif %}
-                    </div>
-                </div>
-            </div>
-        </div>
-        {% endfor %}
-        {% endif %}
-
-        {% endif %}
-
-        {% endif %}
-        {% endfor %}
-    </div>
-    {% elseif c.produtos == 1 %}
-    <div class="px-3 pt-3 title d-flex bg-white">
-        <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
-        <!-- <a class="font-weight-bold ml-auto" href="trending.html">Ver todos <i class="feather-chevrons-right"></i></a> -->
-    </div>
-    <div class="bg-white linha ">
+    {% endif %}
+    {% endfor %}
+</div>
+{% elseif c.produtos == 1 %}
+<div class="px-3 pt-3 title d-flex bg-white">
+    <h5 id="{{ c.slug }}" name="{{ c.slug }}" class="mt-1">{{ c.nome }}</h5>
+    <!-- <a class="font-weight-bold ml-auto" href="trending.html">Ver todos <i class="feather-chevrons-right"></i></a> -->
+</div>
+<div class="bg-white linha ">
     {% for p in produto %}
     {% if c.id == p.id_categoria %}
     {% if hoje in p.dias_disponiveis %}
@@ -262,6 +260,6 @@
             {% endif %}
             {% endfor %}
         </div>
-{% endif %}
-{% endif %}
-{% endfor %}
+        {% endif %}
+        {% endif %}
+        {% endfor %}
