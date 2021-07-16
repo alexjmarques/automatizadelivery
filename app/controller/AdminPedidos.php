4dde9b4442db44a6fdcf8da50f0f412f0fe34fb8
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-07-15 22:18:16 -0300

    Update AdminPedidos.php

--- app/controller/AdminPedidos.php
+++ app/controller/AdminPedidos.php
@@ -429,9 +429,9 @@ class AdminPedidos extends Controller
         //dd($print);
 
         try {
-        $connector = new CupsPrintConnector("STMicroelectronics_POS58_Printer_USB");
+        //$connector = new CupsPrintConnector("STMicroelectronics_POS58_Printer_USB");
         //$connector = new NetworkPrintConnector("159.65.220.187", 9100);
-        //$connector = new NetworkPrintConnector("159.65.220.187", 9100);
+        $connector = new NetworkPrintConnector("159.65.220.187", 9100);
         //$connector = new FilePrintConnector("php://stdout");
         //$connector = new FilePrintConnector("/dev/usb/lp1");
         $printer = new Printer($connector);
