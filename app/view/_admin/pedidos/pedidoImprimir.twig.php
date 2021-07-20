c7f69a99cc308da510356fc768a180d05b8c970e
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-07-20 18:40:31 -0300

    Update pedidoImprimir.twig.php

--- app/view/_admin/pedidos/pedidoImprimir.twig.php
+++ app/view/_admin/pedidos/pedidoImprimir.twig.php
@@ -44,6 +44,44 @@
                     <h4 style="font-family: sans-serif; font-size: 22px;">Nº {{pedido.numero_pedido}}</h4>
                 </td>
             </tr>
+            <tr>
+                <td align="center">
+                    <p style="font-family: sans-serif; font-size: 14px;">Data pedido {{pedido.data_pedido|date("d/m/Y")}}</p>
+                </td>
+            </tr>
+        </table>
+
+        <table style="width:100%;  margin-top:10px; border-top: 2px double #000; font-family: sans-serif; font-size: 14px;">
+            <tr>
+                <td align="left" style="padding-top:10px;"><strong>DADOS CLIENTE</strong></td>
+            </tr>
+
+        </table>
+        <table style="width:100%;  margin-top:5px; font-family: sans-serif; font-size: 13px;">
+            <tr>
+                <td align="left">Cliente: {{cliente.nome}}</td>
+
+            </tr>
+            <tr>
+                <td align="left" style="padding-bottom: 10px;">Telefone: ({{ cliente.telefone[:2] }}) {{ cliente.telefone|slice(2, 5) }}-{{ cliente.telefone|slice(7, 9) }}</td>
+            </tr>
+
+            <tr>
+                <td align="left" style="border-top: 1px dotted #000; border-bottom: 1px solid #ccc; padding-bottom: 10px; font-family: sans-serif; font-size: 13px;">
+                    {% if pedido.tipo_frete == 1 %}
+                    <h4 style="margin: 15px 0 3px 0; padding:0;">Cliente vai retirar</h4>
+                    {% else %}
+                    <h4 style="margin: 15px 0 3px 0; padding:0;">Dados para entrega:</h4>
+                    <strong>Endereço:</strong> {{endereco.rua}}, {{endereco.numero}}<br />
+                    {% if endereco.complemento is not null %}
+                    <strong>Complemento:</strong> {{endereco.complemento}}<br />
+                    {% endif %}
+                    <strong>Bairro:</strong> {{endereco.bairro}} <br />
+                    <strong>CEP:</strong> {{endereco.cep}}
+                    {% endif %}
+
+                </td>
+            </tr>
         </table>
 
         <table style="width:100%;  margin-top:5px; border-top: 2px double #000; font-family: sans-serif; font-size: 14px;">
@@ -155,41 +193,6 @@
                 <td>Cliente Pediu Nota Fiscal Paulista no CPF <strong>{{nf_paulista.cpf}}</strong></td>
             </tr>
             {% endif %}
-        </table>
-
-    
-
-        <table style="width:100%;  margin-top:10px; border-top: 2px double #000; font-family: sans-serif; font-size: 14px;">
-            <tr>
-                <td align="left" style="padding-top:10px;"><strong>DADOS CLIENTE</strong></td>
-            </tr>
-
-        </table>
-        <table style="width:100%;  margin-top:5px; font-family: sans-serif; font-size: 13px;">
-            <tr>
-                <td align="left">Cliente: {{cliente.nome}}</td>
-
-            </tr>
-            <tr>
-                <td align="left" style="padding-bottom: 10px;">Telefone: ({{ cliente.telefone[:2] }}) {{ cliente.telefone|slice(2, 5) }}-{{ cliente.telefone|slice(7, 9) }}</td>
-            </tr>
-
-            <tr>
-                <td align="left" style="border-top: 1px dotted #000; border-bottom: 1px solid #ccc; padding-bottom: 10px; font-family: sans-serif; font-size: 13px;">
-                    {% if pedido.tipo_frete == 1 %}
-                    <h4 style="margin: 15px 0 3px 0; padding:0;">Cliente vai retirar</h4>
-                    {% else %}
-                    <h4 style="margin: 15px 0 3px 0; padding:0;">Dados para entrega:</h4>
-                    <strong>Endereço:</strong> {{endereco.rua}}, {{endereco.numero}}<br />
-                    {% if endereco.complemento is not null %}
-                    <strong>Complemento:</strong> {{endereco.complemento}}<br />
-                    {% endif %}
-                    <strong>Bairro:</strong> {{endereco.bairro}} <br />
-                    <strong>CEP:</strong> {{endereco.cep}}
-                    {% endif %}
-
-                </td>
-            </tr>
         </table>
 
         <table style="width:100%;  margin-top:10px; border-top: 2px double #000; font-family: sans-serif; font-size: 14px;">
