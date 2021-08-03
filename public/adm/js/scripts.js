82bb1468a7cc670b10f1046c10991ad483ecdfee
Alex Marques <37673094+alexjmarques@users.noreply.github.com>
2021-08-03 19:53:19 -0300

    test

--- public/adm/js/scripts.js
+++ public/adm/js/scripts.js
@@ -1902,58 +1902,59 @@ $("#formFinish").submit(function() {
 let autocomplete;
 let address1Field;
 
-$("#ship-address").on('click blur touchleave touchcancel', function() {
+$("#ship-address").on('click blur touchleave touchcancel',
 
-    //function initAutocomplete() {
-    address1Field = document.querySelector("#ship-address");
-    autocomplete = new google.maps.places.Autocomplete(address1Field, {
-        componentRestrictions: {
-            country: ["br", "br"]
-        },
-        fields: ["address_components", "geometry"],
-        types: ["address"],
-    });
-    address1Field.focus();
-    autocomplete.addListener("place_changed", fillInAddress);
-})
+    function initAutocomplete() {
+        address1Field = document.querySelector("#ship-address");
+        autocomplete = new google.maps.places.Autocomplete(address1Field, {
+            componentRestrictions: {
+                country: ["br", "br"]
+            },
+            fields: ["address_components", "geometry"],
+            types: ["address"],
+        });
+        address1Field.focus();
+        autocomplete.addListener("place_changed", fillInAddress);
+    },
 
-function fillInAddress() {
-    const place = autocomplete.getPlace();
+    function fillInAddress() {
+        const place = autocomplete.getPlace();
 
-    for (const component of place.address_components) {
-        const componentType = component.types[0];
-        switch (componentType) {
-            case "street_number":
-                {
-                    $("#numero").val(component.long_name);
+        for (const component of place.address_components) {
+            const componentType = component.types[0];
+            switch (componentType) {
+                case "street_number":
+                    {
+                        $("#numero").val(component.long_name);
+                        break;
+                    }
+                case "route":
+                    {
+                        $("#rua").val(component.long_name);
+                        break;
+                    }
+                case "postal_code":
+                    {
+                        $("#cep").val(component.long_name);
+                        break;
+                    }
+                case "administrative_area_level_2":
+                    {
+                        $("#cidade").val(component.long_name);
+                        break;
+                    }
+                case "administrative_area_level_1":
+                    {
+                        $("#estado").val(component.short_name);
+                        break;
+                    }
+                case "sublocality_level_1":
+                    $("#bairro").val(component.short_name);
                     break;
-                }
-            case "route":
-                {
-                    $("#rua").val(component.long_name);
-                    break;
-                }
-            case "postal_code":
-                {
-                    $("#cep").val(component.long_name);
-                    break;
-                }
-            case "administrative_area_level_2":
-                {
-                    $("#cidade").val(component.long_name);
-                    break;
-                }
-            case "administrative_area_level_1":
-                {
-                    $("#estado").val(component.short_name);
-                    break;
-                }
-            case "sublocality_level_1":
-                $("#bairro").val(component.short_name);
-                break;
+            }
         }
     }
-}
+)
 
 $('#entrega').on('change', function() {
     if (parseInt($(this).val()) === 1) {
